268d46953186f21c5458d1dc1881cccb515511d4
tomasfrancisco <mail@tomasfrancisco.com>
2015-10-24 20:38:55 +0100

    updated without animations

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -7,16 +7,4 @@ $(document).ready(function() {
 	$('#scroll-down-to-contact').click(function() {
 	    $('#contact').animatescroll();
 	});
-
-	$('#scroll-down-to-contact').waypoint(function() {
-		$('#scroll-down-to-contact').addClass('fadeIn');
-	}, {offset : '100%'});
-
-	$(".about-container article").waypoint(function() {
-    	$('.about-container article').addClass('fadeInLeft');
-  	}, { offset: '80%'});
-
-	$(".contact-container article").waypoint(function() {
-    	$('.contact-container article').addClass('fadeInLeft');
-  }, { offset: '80%'});
 });
