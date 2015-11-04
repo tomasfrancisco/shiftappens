859bf37c679a2a3559b9d51689a6dab591ba2fb9
tomasfrancisco <mail@tomasfrancisco.com>
2015-11-04 20:51:26 +0000

    fix video width and set animation delay

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -8,22 +8,24 @@ $(document).ready(function() {
 	    $('#contact').animatescroll();
 	});
 
-	$('#home .shift-date').animate({
-		"margin-bottom" : "0%",
-		"font-size" : "1.2rem"
-	}, 1000, function() {
-		$('#home .video').show(1000);
-		$('#home .video').animate({
-			opacity: 1,
-		}, 1000);
-	});	
-	$('#home .logo').animate({
-		"margin-left": "20%",
-		"margin-right": "20%",
-		"margin-bottom": "1%"
-	}, 1000);	
+	setTimeout(function() {
+		$('#home .shift-date').animate({
+			"margin-bottom" : "0%",
+			"font-size" : "1.2rem"
+		}, 1000, function() {
+			$('#home .video').show(1000);
+			$('#home .video').animate({
+				opacity: 1,
+			}, 1000);
+		});	
+		$('#home .logo').animate({
+			"margin-left": "20%",
+			"margin-right": "20%",
+			"margin-bottom": "1%"
+		}, 1000);	
 
-	$('#home .scroll-btn').animate({
-		"margin-top": "1%"
-	}, 1000);		
+		$('#home .scroll-btn').animate({
+			"margin-top": "1%"
+		}, 1000);		
+	}, 1000);
 });
