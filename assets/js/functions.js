fb54792b7cb85079962197145c5e7b0e05401605
tomasfrancisco <mail@tomasfrancisco.com>
2015-11-04 10:37:36 +0000

    video animation

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -7,4 +7,23 @@ $(document).ready(function() {
 	$('#scroll-down-to-contact').click(function() {
 	    $('#contact').animatescroll();
 	});
+
+	$('#home .shift-date').animate({
+		"margin-bottom" : "0%",
+		"font-size" : "1.2rem"
+	}, 1000, function() {
+		$('#home .video').show(1000);
+		$('#home .video').animate({
+			opacity: 1,
+		}, 1000);
+	});	
+	$('#home .logo').animate({
+		"margin-left": "20%",
+		"margin-right": "20%",
+		"margin-bottom": "1%"
+	}, 1000);	
+
+	$('#home .scroll-btn').animate({
+		"margin-top": "1%"
+	}, 1000);		
 });
