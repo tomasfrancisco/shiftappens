0b7e75cb20adedfbd24b8bc0645ab32f6d8066e1
tomasfrancisco <mail@tomasfrancisco.com>
2015-12-07 21:44:42 +0000

    update website version

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -28,4 +28,68 @@ $(document).ready(function() {
 			"margin-top": "1%"
 		}, 1000);		
 	}, 1000);
+
+	$('#schedule-prev').click(function() {
+		var prev = $('#schedule-prev');
+		var next = $('#schedule-next');
+
+		var friday = $('#schedule-friday');
+		var saturday = $('#schedule-saturday');
+		var sunday = $('#schedule-sunday');
+
+		if(!$(saturday).is(':visible')) {
+			$(sunday).hide();
+			$(saturday).css('display', 'inline-block');
+		} else if(!$(sunday).is(':visible') && !$(friday).is(':visible')) {
+			$(saturday).hide();
+			$(friday).css('display', 'inline-block');
+			$(prev).attr('disabled', true);
+			$(prev).css('opacity', '0.1');
+		} else if(!$(friday).is(':visible') && !$(saturday).is(':visible')) {
+			$(saturday).hide()
+			$(friday).css('display', 'inline-block');
+			$(prev).attr('disabled', true);
+			$(prev).css('opacity', '0.1');
+		} else if($(sunday).is(':visible') && $(saturday).is(':visible') ) {
+			$(sunday).hide()
+			$(friday).css('display', 'inline-block');
+			$(prev).attr('disabled', true);
+			$(prev).css('opacity', '0.1');
+		}
+
+		$(next).attr('disabled', false);
+		$(next).css('opacity', '1');
+	});
+
+	$('#schedule-next').click(function() {
+		var prev = $('#schedule-prev');
+		var next = $('#schedule-next');
+
+		var friday = $('#schedule-friday');
+		var saturday = $('#schedule-saturday');
+		var sunday = $('#schedule-sunday');
+
+		if(!$(saturday).is(':visible') && !$(sunday).is(':visible')) {
+			$(friday).hide();
+			$(saturday).css('display', 'inline-block');
+		} else if(!$(friday).is(':visible') && !$(sunday).is(':visible')) {
+			$(saturday).hide();
+			$(sunday).css('display', 'inline-block');
+			$(next).attr('disabled', true);
+			$(next).css('opacity', '0.1');
+		} else if(!$(sunday).is(':visible') && !$(saturday).is(':visible')) {
+			$(saturday).hide()
+			$(sunday).css('display', 'inline-block');
+			$(next).attr('disabled', true);
+			$(next).css('opacity', '0.1');
+		} else if($(friday).is(':visible') && $(saturday).is(':visible')) {
+			$(friday).hide()
+			$(sunday).css('display', 'inline-block');
+			$(next).attr('disabled', true);
+			$(next).css('opacity', '0.1');
+		}
+
+		$(prev).attr('disabled', false);
+		$(prev).css('opacity', '1');
+	});
 });
