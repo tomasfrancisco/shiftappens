dcdc5deff36e997aa117531382a29869581c5e3b
Pedro Janeiro <pedro_janeirito@hotmail.com>
2016-01-09 06:38:42 +0000

    Sign-in now allows editing answers after submission

--- assets/js/signin.js
+++ assets/js/signin.js
@@ -8,16 +8,12 @@ $(document).ready(function() {
 
     var askFaculty = function() {
         $("#workplace").collapse('hide');
-        $("#workplaceInput").prop('required',false);
         $("#faculty").collapse('show');
-        $("#facultyInput").prop('required',true);
     };
 
     var askWorkPlace = function() {
         $("#faculty").collapse('hide');
-        $("#facultyInput").prop('required',false);
         $("#workplace").collapse('show');
-        $("#workplaceInput").prop('required',true);
     };
 
     $("#student").on("click", askFaculty);
