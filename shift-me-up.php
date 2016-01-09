ec73dc6f409509f2eb3f2297fd83fbf823054472
Pedro Janeiro <pedro_janeirito@hotmail.com>
2016-01-09 13:45:14 +0000

    Tiny :poop:

--- shift-me-up.php
+++ shift-me-up.php
@@ -286,7 +286,7 @@ if(isset($_GET) and isset($_GET['id'])) {
                                     </div>
                                 </div>
                                 <label for="otherSkillsInput" class="control-label">Outro:</label>
-                                <input type="text" class="form-control" name="otherSkills" id="otherSkillsInput" placeholder="Awesome skill" <?php if($edit){echo "value=\"".$otherSkills['skill']."\"";} ?>/>
+                                <input type="text" class="form-control" name="otherSkills" id="otherSkillsInput" placeholder="Awesome skill" <?php if($edit){echo "value=\"".$otherSkill['skill']."\"";} ?>/>
                             </div>
                             <div class="col-sm-12 col-md-6">
                                 <p>Quais são as tuas áreas?</p>
