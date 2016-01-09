fc8552cbc9c3d6d6a29aa612b1246075b381b417
Pedro Janeiro <pedro_janeirito@hotmail.com>
2016-01-09 13:00:23 +0000

    :poop:

--- shift-me-up.php
+++ shift-me-up.php
@@ -14,45 +14,45 @@ if(mysqli_connect_errno() == 0) {
     $query = $db->prepare("SELECT * FROM hashcodes WHERE hash = ?;");
     $query->bind_param("s",$hash);
 
-    $result = $query->execute();
+    $stmt = $query->execute();
     if($result !== false) {
         $edit = true;
 
-        $result = $result->get_result();
+        $result = $stmt->get_result();
         $row = $result->fetch_array();
         $email = $row['email'];
 
         $query = $db->prepare("SELECT * FROM entries WHERE email = ?;");
         $query->bind_param("s",$email);
-        $result = $query->execute();
-        $result = $result->get_result();
+        $stmt = $query->execute();
+        $result = $stmt->get_result();
         $entries = $result->fetch_array();
 
         $query = $db->prepare("SELECT * FROM areas WHERE email = ?;");
         $query->bind_param("s",$email);
-        $result = $query->execute();
-        $result = $result->get_result();
+        $stmt = $query->execute();
+        $result = $stmt->get_result();
         while($row = $result->fetch_array()) {
             $areas[] = $row["area"];
         }
 
         $query = $db->prepare("SELECT * FROM skills WHERE email = ?;");
         $query->bind_param("s",$email);
-        $result = $query->execute();
-        $result = $result->get_result();
+        $stmt = $query->execute();
+        $result = $stmt->get_result();
         while($row = $result->fetch_array()) {
             $skills[] = $row["skill"];
         }
 
         $query = $db->prepare("SELECT * FROM otherSkills WHERE email = ?;");
         $query->bind_param("s",$email);
-        $result = $query->execute();
-        $result = $result->get_result();
+        $stmt = $query->execute();
+        $result = $stmt->get_result();
         $otherSkill = $result->fetch_array();
 
         $query = $db->prepare("SELECT * FROM frameworks WHERE email = ?;");
         $query->bind_param("s",$email);
-        $result = $query->execute();
+        $stmt = $query->execute();
 
         while($row = $result->fetch_array()) {
             $frameworks[] = $row["framework"];
