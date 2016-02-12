032bc4a7bcc2ad63f995852368e135a1f6af092c
Pedro Janeiro <pedro_janeirito@hotmail.com>
2016-02-12 18:48:10 +0000

    Selecting everyone

--- admin.php
+++ admin.php
@@ -36,7 +36,21 @@ if(!isset($_SESSION['logged'])) {
         <title>ShiftAdmin</title>
     </head>
     <body>
-        
+    <?php
+    $link = mysql_connect('localhost', 'shiftappens', 'ianchLansn_*a1?zJHAmaxvy');
+    $db = mysql_select_db('shiftappens2016',$link);
+    $emails_query = mysql_query("SELECT email FROM entries;");
+    while($emails_result = mysql_fetch_array($emails_query)) {
+        $email = $emails_result['email'];
+        $entries_query = mysql_query("SELECT * FROM entries WHERE email='{$email}';");
+        $entry = mysql_fetch_array($entries_query);
+        print("
+        <div class=\"participant\">
+            <p>{$entry['name']}</p>
+        </div>
+        ");
+    }
+    ?>
     </body>
     </html>
     <?php
