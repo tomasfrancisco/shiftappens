aa512c877389e4893cfff30d3cdda2f11ddc31ee
Pedro Janeiro <pedro_janeirito@hotmail.com>
2016-02-18 04:36:16 +0000

    First version of countdown

---
+++ assets/js/countdown.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+    var main;
+    var max_time = 10;
+    var time = max_time;
+    $("#time").text(time);
+    $("#stop_timer").css("opacity", "0.5");
+    $("#stop_timer").attr("disabled", true);
+
+    function startTimer() {
+        time = max_time;
+        main = setInterval(function(){
+            time = time - 1;
+            if(time!=0) {
+                console.log(time);
+                $("#time").text(time);
+            } else {
+                stopTimer();
+                time = max_time;
+                $("#time").text(time);
+            }
+        },1000);
+        $("#start_timer").css("opacity", "0.5");
+        $("#start_timer").attr("disabled", true);
+        $("#stop_timer").css("opacity", "1.0");
+        $("#stop_timer").attr("disabled", false);
+    }
+
+    function stopTimer() {
+        time = 0;
+        $("#time").text(time);
+        clearInterval(main);
+        $("#stop_timer").css("opacity", "0.5");
+        $("#stop_timer").attr("disabled", true);
+        $("#start_timer").css("opacity", "1.0");
+        $("#start_timer").attr("disabled", false);
+    }
+
+    $("#start_timer").click(startTimer);
+    $("#stop_timer").click(stopTimer);
+});
