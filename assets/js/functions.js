c89725b6fc3edcd164e0b9bb4d322252c85918bd
Pedro Janeiro <pedro_janeirito@hotmail.com>
2016-02-19 06:05:30 +0000

    Whatever

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -129,9 +129,7 @@ $(document).ready(function() {
 			limit: 3,
 			get: 'popular',
 	        clientId: '83b980446cfc450eade4beec44388ee5',
-	        error: function(message) {
-	        	alert(message);
-	        }
+	        accessToken: '5f34f1b6acf9414783dd308bd18a5456'
 	    });
 	    feed.run();
 	}, 1000);
